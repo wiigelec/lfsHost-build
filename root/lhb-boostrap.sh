4a6bfeaab7e8e004f9806cbb5b776263b24def2e
wiiglec <wiigelec@hotmail.com>
2021-11-25 09:46:36 -0700

    build scripts

--- root/lhb-boostrap.sh
+++ root/lhb-boostrap.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------
-#
+# boostrap lfsHost build
+
+# build base1
+pushd blfs_base1/work
+make
+popd
 
 
 # END script

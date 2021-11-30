f7f6d7b8c2ea5794fb276935ead1576a10235d26
wiigelec <59587034+wiigelec@users.noreply.github.com>
2021-11-30 08:03:36 -0700

    11.0v1 (#3)
    * kernel build
    
    * kernel build
    
    * core2
    
    * core2
    
    * kernel net
    
    * make mrproper
    
    * filenames
    
    * jhalfs
    
    * jhalfs
    
    * clean
    
    * blfs_root
    
    * kernel-config.full
    
    * remove scripts
    
    * chmod -x wget-list
    
    * full kernel

--- root/build-kernel.sh
+++ root/build-kernel.sh
@@ -6,39 +6,88 @@
 # Get parameter
 type=$1
 
+# usage
+usage=1;
 case $type in
 
     default)
-	    echo "Building default kernel..."
-	    tar -xf /mnt/lfs/sources/linux-5.13.12.tar.xz -C
-	    /mnt/lfs/sources
-	    pushd /mnt/lfs/sources/linux-5.13.12.tar.xz
-	    make defconfig
-	    make -j4
-	    cp .config /boot/config-5.13.12
-	    cp System.map /boot/config/System.map-5.13.12
-	    cp arch/x86_64/bzImage /boot/vmlinuz-5.13.12-lfs-11.0
-	    rm -rf linux-5.13.12
-	    popd
-	    echo "Done."
-	    exit
-	;;
+	    usage=0
+	    ;;
     base)
-	;;
+	    usage=0
+	    ;;
     net)
-	;;
+	    usage=0
+	    ;;
     full)
-	;;
+	    usage=0
+	    ;;
     *)
-	echo "Usage: ./build-kernel {default,base,net,full}"
-	;;
+	    usage=1
+	    ;;
 esac
 
+# wrong parameters
+if [ $usage -eq 1 ]; then
+	echo "Usage: ./build-kernel {default,base,net,full}"
+	exit
+fi
+
+# extract tarball and clean
+echo "Extracting /sources/linux-5.13.12.tar.xz..."
+tar -xf /sources/linux-5.13.12.tar.xz -C /sources
+echo "Cleaning source tree..."
+pushd /sources/linux-5.13.12
+make mrproper
+popd
+
+# default
+if [ $type == "default" ]; then
+	echo "Building default kernel..."
+	pushd /sources/linux-5.13.12
+	make defconfig
+	popd
+fi
+
+# base
+if [ $type == "base" ]; then
+        echo "Building base kernel..."
+	#copy config
+	cp /root/kernel-config.base /sources/linux-5.13.12/.config
+fi
+
+# net
+if [ $type == "net" ]; then
+        echo "Building net kernel..."
+	#copy config
+	cp /root/kernel-config.net /sources/linux-5.13.12/.config
+fi
+
+# full
+if [ $type == "full" ]; then
+        echo "Building full kernel..."
+	#copy config
+	cp /root/kernel-config.full /sources/linux-5.13.12/.config
+fi
 
 
+# build kernel
+pushd /sources/linux-5.13.12
+make -j4
 
+# install modules
+make modules_install
 
+# copy files
+cp -v .config /boot/config-5.13.12
+cp -v System.map /boot/System.map-5.13.12
+cp -v arch/x86_64/boot/bzImage /boot/vmlinuz-5.13.12-lfs-11.0
 
+# cleanup
+rm -rf linux-5.13.12
+popd
+echo "Done."
+exit
 
 
 # END script
